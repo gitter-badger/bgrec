4bf4c634a71aeb4d0c8f6399a550876b17778361
davidkaste <d.castella.85@gmail.com>
2014-11-20 23:24:23 +0100

    Added test for BoardGameRetriever class

---
+++ src/test/java/com/davidcastella/bgrec/scraper/BoardGameRetrieverTest.java
@@ -0,0 +1,38 @@
+package com.davidcastella.bgrec.scraper;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.net.URL;
+
+import com.davidcastella.bgrec.scraper.fixtures.BoardGameFixture;
+
+import static org.junit.Assert.*;
+
+public class BoardGameRetrieverTest {
+
+    BoardGameRetriever bgr;
+
+    @Before
+    public void setUp() throws Exception {
+        bgr = new BoardGameRetriever(new URL("http://boardgamegeek.com/xmlapi/boardgame/"), "13", "XML");
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void testRetrieveSlashEndedUri() throws Exception {
+        Assert.assertEquals(bgr.retrieve(), BoardGameFixture.getSettlersOfCatan());
+    }
+
+    @Test
+    public void testRetrieveNonSlashEndedUri() throws Exception {
+        bgr.setBaseUri(new URL("http://boardgamegeek.com/xmlapi/boardgame"));
+        Assert.assertEquals(bgr.retrieve(), BoardGameFixture.getSettlersOfCatan());
+    }
+}
