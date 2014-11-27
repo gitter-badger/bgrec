01439d0c9a52f1bf1a659937f297b91ac000f6a5
davidkaste <d.castella.85@gmail.com>
2014-11-27 20:28:47 +0100

    Added BoardgameRetrieverTest

---
+++ src/test/java/com/davidcastella/bgrec/scraper/BoardGameRetrieverTest.java
@@ -0,0 +1,38 @@
+package com.davidcastella.bgrec.scraper;
+
+import com.davidcastella.bgrec.scraper.exceptions.NonExistentPropertyException;
+import org.joox.Match;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.net.URL;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class BoardGameRetrieverTest {
+    BoardGameRetriever bgr;
+
+    @Before
+    public void setUp() throws Exception {
+        bgr = new BoardGameRetriever(new URL("http://boardgamegeek.com/xmlapi/boardgame"), "3076");
+    }
+
+    @Test
+    public void testQuery() throws Exception {
+        List<Match> x = bgr.query("name");
+        assertEquals("Puerto Rico", x.get(0).xpath("//name[@primary = 'true']").content());
+        assertEquals("2002", bgr.query("yearpublished").get(0).content());
+    }
+
+    @Test
+    public void testQueryNonExistentPropertyException() {
+        boolean thrown = false;
+        try {
+            bgr.query("nonexistent");
+        } catch (NonExistentPropertyException nep) {
+            thrown = true;
+        }
+        assertTrue(thrown);
+    }
+}
